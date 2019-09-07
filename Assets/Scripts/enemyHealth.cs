5a12aa6771d155035505b3bc6e6631f7bb5235f7
Giovanni Trevisan <gitrevizzo@hotmail.com.br>
2019-09-07 16:29:35 -0300

    Movimentacao do inimigo

---
+++ Assets/Scripts/enemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    public int StartHealth = 1;
+    public int scorePoint = 1;
+
+    bool EnemyisDead = false;
+    int CurrentHealth;
+
+    void Start()
+    {
+        CurrentHealth = StartHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeHealth(int amount)
+    {
+
+        if (EnemyisDead == false)
+        {
+
+            CurrentHealth = CurrentHealth + amount;
+            if (CurrentHealth <= 0)
+            {
+                EnemyisDead = true;
+
+                Destroy(gameObject, 2f);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour
+{
+    public GameObject[] emptyObjects;
+    public float speedRotation = 1f;
+    public float thrust = 20f;
+    public float maxSpeed = 10f;
+
+    public int IndexObject;
+    int indexPrev;
+    Rigidbody Rigidbody;
+
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        IndexObject = Random.Range(0, (emptyObjects.Length));
+        StartCoroutine(mudarDirecao(3f));
+    }
+
+    private void Update()
+    {
+        Rigidbody.velocity = Vector3.ClampMagnitude(Rigidbody.velocity, maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+
+        //transform.rotation(0f, transform.rotation.y, 0f);
+
+        var rotation = Quaternion.LookRotation(emptyObjects[IndexObject].transform.position - transform.position);
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speedRotation);
+        //transform.LookAt(emptyObjects[IndexObject].transform);
+        Rigidbody.AddForce(transform.forward * thrust);
+    }
+
+    public void SwitchIndex() 
+    {
+        IndexObject = Random.Range(0, (emptyObjects.Length));
+        if (emptyObjects.Length > 1)
+        {
+            do
+            {
+                IndexObject = Random.Range(0, emptyObjects.Length);
+            } while (IndexObject == indexPrev);
+        }
+        indexPrev = IndexObject;
+    }
+
+    IEnumerator mudarDirecao(float time)
+    {
+        SwitchIndex();
+        yield return new WaitForSeconds(time);
+
+        StartCoroutine(mudarDirecao(Random.Range(1f, 4f)));
+    }
+
+}
