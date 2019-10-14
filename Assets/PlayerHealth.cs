dce6ddaa8b33c4d7eea88c2e36fc57bac14034d7
Giovanni Trevisan <gitrevizzo@hotmail.com.br>
2019-10-14 00:04:21 -0300

    alpha finalizado

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public static int life;
+
+    public GameObject canvasGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(life <= 0)
+        {
+            canvasGameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
